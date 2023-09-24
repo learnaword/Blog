ff19ebb60d21d9b83eb0c403ea72bf82b3681263
majunliang <875657453@qq.com>
2023-09-24 18:38:01 +0800

    自动生成改成无图模式

--- src/main/java/com/mjl/blog/service/admin/auto/blog/AutoBlogServiceAdminImpl.java
+++ src/main/java/com/mjl/blog/service/admin/auto/blog/AutoBlogServiceAdminImpl.java
@@ -31,13 +31,14 @@ public class AutoBlogServiceAdminImpl implements AutoBlogAdminService {
     @Override
     public void create(CreateReqVO createReqVO) {
         //从数据库获取两个随机句子。
-        List<SentenceDO> sentenceDOList =  sentenceService.getListLimit(2);
-
+        List<SentenceDO> sentenceDOList =  sentenceService.getListLimit(1);
         //根据文章标题autoConfig获取到配置信息。
         AutoConfigDO autoConfigDO = autoConfigService.getByBlogTitle(createReqVO.getTitle());
+
         if(createReqVO.getAutoConfig() !=0){
             autoConfigDO = autoConfigService.getById(createReqVO.getAutoConfig());
         }
+
         if(autoConfigDO==null){
             //异常，没有可用模版
             throw exception(INVALID_TEMPLATE);
@@ -45,25 +46,15 @@ public class AutoBlogServiceAdminImpl implements AutoBlogAdminService {
 
         SoftDO softDO = softService.getSoftById(autoConfigDO.getSoftId());
         String contentImges = "";
+
         if(!createReqVO.getContent().contains("img")){
             contentImges ="<img src=\"" +autoConfigDO.getContentImages()+ "\" style=\"width: 339.486px; float: none;\" class=\"imageCenter\"><br>";
         }
+
         String[] sentenceHead =sentenceDOList.get(0).getContent().split("，");
         String introduction =autoConfigDO.getTitle() + sentenceHead[0] + "，" + sentenceHead[1] + "，下面回答的问题是“" + createReqVO.getTitle() + "”，希望可以帮到你。";
-        String contentHtml = "<p>" + softDO.getTitle() + sentenceHead[0] + "，" + sentenceHead[1] + "，下面回答的问题是“" + createReqVO.getTitle() + "”，希望可以帮到你。" +
-                "<h2 id=\"nav1_1\">" + createReqVO.getTitle() +
-                "</h2><div>" +
-                autoConfigDO.getRecommendHtml()+"<br>" +
-                "<span style=\"font-weight: bold;\">问题答案</span><br><br>" +
-                createReqVO.getContent() +
-                "<br>"+
-                contentImges +
-                "以上就是" +
-                createReqVO.getTitle() +
-                "的全部内容了，" +
-                softDO.getTitle()  +
-                sentenceDOList.get(1).getContent() +
-                "。</div>";
+        String contentHtml = getContentHtml(softDO,sentenceHead,createReqVO,autoConfigDO,contentImges,false);
+
         BlogDO blogDO = new BlogDO();
         blogDO.setImages( autoConfigDO.getImages() );
         blogDO.setRankScore( autoConfigDO.getRankScore() );
@@ -83,5 +74,30 @@ public class AutoBlogServiceAdminImpl implements AutoBlogAdminService {
 
         //更新句子使用次数
         sentenceService.updateList(sentenceDOList);
+    }
+
+    public String getContentHtml(SoftDO softDO,String[] sentenceHead,CreateReqVO createReqVO,AutoConfigDO autoConfigDO,String contentImges,boolean hasImage){
+        String contentHtml = "<p>" + softDO.getTitle() + sentenceHead[0] + "，" + sentenceHead[1] + "，下面回答的问题是“" + createReqVO.getTitle() + "”，希望可以帮到你。" +
+                "<h2 id=\"nav1_1\">" + createReqVO.getTitle() +
+                "</h2><div>" +
+                autoConfigDO.getRecommendHtml()+"<br>" +
+                "<span style=\"font-weight: bold;\">问题答案</span><br><br>" +
+                createReqVO.getContent();
+
+        if(hasImage){
+            List<SentenceDO> sentenceDOList =  sentenceService.getListLimit(1);
+            contentHtml += "<br>"+
+                    contentImges +
+                    "以上就是" +
+                    createReqVO.getTitle() +
+                    "的全部内容了，" +
+                    softDO.getTitle()  +
+                    sentenceDOList.get(0).getContent() +
+                    "。</div>";
+        }else{
+            contentHtml += "</div>";
+        }
+
+        return contentHtml;
     }
 }
